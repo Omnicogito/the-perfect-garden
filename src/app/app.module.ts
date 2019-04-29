e41d8454dbd2d7215bae4d21309a82fa63b5eadf
Spriggs01 <sarah.spriggs@gamil.com>
2019-04-29 19:43:32 -0400

    updates for new products components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ThePerfectGardenDetailComponent } from './components/the-perfect-garden/the-perfect-garden-detail/the-perfect-garden-detail.component';
+import { ProductsService } from './services/products.service';
+import { HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
+import {MatTableModule} from '@angular/material';
+
+
+const routes = [
+  {path: 'products', component: ProductsComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProductsComponent,
+    ThePerfectGardenDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes),
+    AppRoutingModule,
+    MatTableModule
   ],
-  providers: [],
+  providers: [
+    ProductsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
