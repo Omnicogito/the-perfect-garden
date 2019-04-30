333388952ab9ffcf8e0e2af0b4eb2d5018c1f9e8
Omnicogito <47541570+Omnicogito@users.noreply.github.com>
2019-04-30 10:39:42 -0400

    Commit again

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing/app-routing.module';
-import { MatToolbarModule } from '@angular/material';
 import { MatButtonModule } from '@angular/material/';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AboutComponent } from './components/about/about.component';
+import { MatToolbarModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+// tslint:disable-next-line: max-line-length
+import { ThePerfectGardenDetailComponent } from './components/the-perfect-garden/the-perfect-garden-detail/the-perfect-garden-detail.component';
+import { ProductsService } from './services/products.service';
+import { RouterModule } from '@angular/router';
+import { MatTableModule } from '@angular/material';
+
+
+const routes = [
+  {path: 'products', component: ProductsComponent},
+];
+
 
 
 @NgModule({
@@ -15,6 +28,7 @@ import { AboutComponent } from './components/about/about.component';
     AppComponent,
     HeaderComponent,
     AboutComponent,
+    ProductsComponent,
   ],
   imports: [
     BrowserModule,
@@ -22,9 +36,14 @@ import { AboutComponent } from './components/about/about.component';
     MatButtonModule,
     ReactiveFormsModule,
     AppRoutingModule,
-    HttpClientModule,
+    ProductsComponent,
+    ThePerfectGardenDetailComponent,
+    HomeComponent
+
   ],
-  providers: [],
+  providers: [
+    ProductsService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
